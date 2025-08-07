a8aaac68df0d27b7b8837b877ca3c606d1978fa9
a1t0git <github_ctc101@a1t0.aleeas.com>
2025-08-07 18:42:30 -0500

    feat: add captured friend's name to HTML list.
    Continued development of capturing a friend's name and adding to a list. So far created function 'addFriendToList()', although still missing adding a proper ID to the HTML tag, for identification purposes. Global idea, to replace this funciton by a more general 'updateList()', that recreates HTML list every single time with full JS array 'friends', instead of modifying 1 record at a time. Feature still in development.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //  VARIABLES DECLARATION.
+//  'conts' vs. 'let' vs. 'var': 'https://www.freecodecamp.org/news/var-let-and-const-whats-the-difference/'.
 let friends = [];
 
 //  FUNCTIONS DECLARATION.
@@ -8,51 +9,83 @@ let friends = [];
 //  Ref.: 'https://superuser.com/questions/1037389/innerhtml-vs-value-when-and-why',
 //+ 'https://stackoverflow.com/questions/8823498/setting-innerhtml-vs-setting-value-with-javascript' .
 function getValue(iIsProperty = false, iID) {
+    
     let html_object_value = null;
-    let html_object = document.getElementById(iID); // e.g. Methods to get value of 'input' HTML tag: 'https://stackoverflow.com/questions/11563638/how-do-i-get-the-value-of-text-input-field-using-javascript/11563667#11563667'.
+    const html_object = document.getElementById(iID); // e.g. Methods to get value of 'input' HTML tag: 'https://stackoverflow.com/questions/11563638/how-do-i-get-the-value-of-text-input-field-using-javascript/11563667#11563667'.
     (iIsProperty === false) ? html_object_value = html_object.innerHTML : html_object_value = html_object.value;
     return html_object_value;
+
 }
 
 //  Set HTML tag's value: if 'innerHTML'; e.g. "<div>some text</div>".
 //+ Otherwise, 'value' tag's property; e.g. "<input value='some text' />".
 function setValue(iIsProperty = false, iID, iValue) {
-    let html_object = document.getElementById(iID);
+
+    const html_object = document.getElementById(iID);
     (iIsProperty === false) ? html_object.innerHTML = iValue : html_object.value = iValue;
     return;
+
 }
 
 function initialize() {
 
 }
 
+// function updateList(iHTMLList, iList) {
+// }
+function addFriendToList(iID, iFriend) {
+
+    const friend_html = document.createElement('li');
+    // //  OP01: direct not-safe HTML text injection.
+    // friend_html.innerHTML = iFriend;
+    //  OP02: code-safe, with '.createTextNode'.
+    const friend_html_text = document.createTextNode(iFriend);
+    friend_html.appendChild(friend_html_text);
+    
+    const friends_html = document.getElementById(iID);
+    friends_html.appendChild(friend_html);
+
+}
+
 function agregarAmigo() {
+
+    //  VARIABLES DECLARATION.
     let friend_valid = false;
     let friend_valid_string = false;
     let friend_valid_not_in_list = false;
 
-    let friend = getValue(iIsProperty = true, 'amigo');
+    //  'conts' vs. 'let' vs. 'var': 'https://www.freecodecamp.org/news/var-let-and-const-whats-the-difference/'.
+    const friend = getValue(iIsProperty = true, 'amigo');
     // console.log(`friend: '${friend}'`);
 
-    //  Validations.
-    //  Validation 01: not empty or whitespace-only string. Ref. 'https://medium.com/@muhebollah.diu/crafting-a-powerful-empty-value-checker-in-javascript-6a0bbef2c1a4'.
+    //  VALIDATIONS.
+    //  VALIDATION 01: not empty or whitespace-only string. Ref. 'https://medium.com/@muhebollah.diu/crafting-a-powerful-empty-value-checker-in-javascript-6a0bbef2c1a4'.
     if (friend.trim().length !== 0) {friend_valid_string = true;}
     // console.log(`friend_valid_string: '${friend.trim().length}' | '${friend_valid_string}'`);
-    //  Validation 02: friend not in list. Ref. 'https://stackoverflow.com/questions/48592791/javascript-arrays-opposite-of-includes/75187084#75187084'.
+    //  VALIDATION 02: friend not in list. Ref. 'https://stackoverflow.com/questions/48592791/javascript-arrays-opposite-of-includes/75187084#75187084'.
     if (friends.indexOf(friend) === -1) {friend_valid_not_in_list = true;}
     // console.log(`friend_valid_not_in_list: '${friends.indexOf(friend)}' | '${friend_valid_not_in_list}'`);
-    // All validations passed?
+    // VALIDATION - LAST: all previous validations passed?
     if (friend_valid_string && friend_valid_not_in_list) {friend_valid = true;}
     // console.log(`friend_valid: '${friend_valid}'`);
 
+    //  EXECUTION.
     //  If input validations pass, add friend to friends list.
-    if (friend_valid) {friends.push(friend);}
-    // console.log(friends);
+    if (friend_valid) {
+        friends.push(friend);
+        addFriendToList('listaAmigos', friends[friends.length - 1]); // UPDATE 02, more granular.
+    }
+    console.log(`friends: '${friends}'`);
+    console.log(friends);
 
-    //  Updates triggered.
-    
-    //  Clean input tag 'amigo'. Ref. 'https://www.geeksforgeeks.org/html/html-clearing-the-input-field/'.
+    //  UPDATES TRIGERED.
+
+    //  UPDATE 01: clean input tag 'amigo'. Ref. 'https://www.geeksforgeeks.org/html/html-clearing-the-input-field/'.
     setValue(iIsProperty = true, 'amigo', '');
+
+    //  UPDATE 02: add new friend to HTML list.
+    // updateList('listaAmigos', friends);
+
 }
 
 function sortearAmigo() {
